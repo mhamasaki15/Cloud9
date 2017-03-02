1cb718e215f6e0c8857f05566a82e46eac371f9c
student <mhamasak@usc.edu>
2017-03-01 23:00:25 -0800

    finished word cloud list generation

--- test/app/Http/Controllers/MyController.php
+++ test/app/Http/Controllers/MyController.php
@@ -32,39 +32,41 @@ class MyController extends Controller {
 	'base_uri' => 'http://api.musixmatch.com/ws/1.1/',
 	'timeout' => 2.0
      ]);
-     $response = $client->get('track.search?f_artist_id=' . $artistId . '&page_size=10&page=1&f_lyrics_language=en&f_has_lyrics=true' . $this->verification);
+     $response = $client->get('track.search?f_artist_id=' . $artistId . '&page_size=25&page=1&f_lyrics_language=en&f_has_lyrics=true' . $this->verification);
      $trackList = json_decode($response->getBody(), true);
      $trackList = $trackList['message']['body']['track_list'];
 
-     $allSongLyrics = "";
+     $allSongLyrics = " ";
      for ($i = 0; $i < count($trackList); $i++){
        $response = $client->get('track.lyrics.get?track_id=' . $trackList[$i]['track']['track_id'] . $this->verification);
        $songLyrics = json_decode($response->getBody(), true);
        $songLyrics = $songLyrics['message']['body']['lyrics']['lyrics_body'];
 
        //getting rid of common stuff.
-       $toReplace = array(".", ")", "(", "\"", "]", "[", "1409614310238", "\n", ',', '******* This Lyrics is NOT for Commercial use *******');
+       $toReplace = array(".", ")", "(", "\"", "]", "[", "1409614310238", "\n", ',', '******* This Lyrics is NOT for Commercial use *******'); 
        $songLyrics = str_replace($toReplace, " ", $songLyrics);
+       $songLyrics = strtolower($songLyrics);
+
+       $commonEnglishWords = array(" a ", " i ", " i'm ", " it's ", " do ", " am ", " the ", " to ", " in ", " at ", " is ", " it ", " was ", " are ", " that ", " of ", " be ", " at ", " or ", " by ", " this ", " and ", " you ", " me ", " some ", " how ", " my ", " on ", " they ", " get ", " we ", " so ", " but "); 
+       $songLyrics = str_replace($commonEnglishWords, " ", $songLyrics);
     
        //Remove the last word (its useless)
        $songLyrics = substr($songLyrics, 0, strrpos($songLyrics, ' '));
 
        $allSongLyrics = $allSongLyrics . " " . $songLyrics;
-
-       //echo $songLyrics;
      }
 
-     $allSongLyrics = strtolower($allSongLyrics);
      $wordList = explode(" ", $allSongLyrics);
      $wordList = array_filter($wordList);
      $wordList = array_count_values($wordList);
+     arsort($wordList);
+     $wordList = array_slice($wordList, 0, 250, true);
      echo '<pre>';
      print_r($wordList);
      echo '</pre>';
 
-var_dump($wordList);
+     $wordList = json_encode($wordList);
 
-
-
+     //return view('wordcloud', [wordlist => $wordList]);
    }
 }
